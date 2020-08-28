0941dc2aac5d94926b25fb99e708cda2a4881a15
CodeDrummer <code.drummer87@gmail.com>
2020-08-28 18:15:15 +0600

    Open/Closed door function has been added

--- code.js
+++ code.js
@@ -1,3 +1,27 @@
+let music = new Audio('music/main_theme.mp3');
+
 $(document).ready(function() {
-	
+	$('.VL10').on('click', '#door_2', function() {
+		OpenTheDoor("#door_2");
+
+	}).on('click', '#door_1', function() {
+		$(this).remove();
+		OpenTheDoor("#door_1");
+	});
 });
+
+function OpenTheDoor(id) {
+	music.play();
+	$(id).css('display', 'none');
+	let audio = new Audio('sounds/open_door.mp3');
+	audio.play();
+
+	setTimeout(CloseTheDoor, 3000, id);
+}
+
+function CloseTheDoor(id) {
+	let audio = new Audio('sounds/close_door.mp3');
+	audio.play();
+
+	$(id).css('display', 'block');
+}
